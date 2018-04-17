141f553f0f60de3e15133e05a87c0fb2ce67ca3a
David Becht <bechtd@gmail.com>
2018-04-17 18:58:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function theBeatlesPlay() {
     'Paul McCartney plays Bass Guitar',
     'George Harrison plays Lead Guitar',
     'Ringo Starr plays Drums',
-    'Pete Best plays the worlds smallest violin'
+    //'Pete Best plays the worlds smallest violin'
   ];
 }
